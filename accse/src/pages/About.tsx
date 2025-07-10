f743dd8b729f2ac74ad7a638c13fc1f208a93b2d
pamelaewenam <braemmanuel89@gmail.com>
2025-07-10 13:15:26 +0000

    Added sitemaps and robots, and also added helmet tags fro enhanced SEO

--- accse/src/pages/About.tsx
+++ accse/src/pages/About.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet-async';
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import AboutPageBanner from "../components/About/aboutPageBanner";
@@ -12,6 +13,24 @@ function About() {
     return(
         
         <>
+        <Helmet>
+        <title>About ACCSE – Who We Are & What We Stand For</title>
+      <meta  name="description" content="Learn about ACCSE, a youth-led nonprofit in Ghana empowering communities through sustainable climate solutions, education, and innovation." />
+      <meta name="keywords" content="ACCSE, about ACCSE, climate nonprofit Ghana, youth-led organization, women in sustainability, community empowerment, environmental education"/>
+      <meta name="author" content="Africa Center for Climate and Sustainability Empowerment" />
+      <meta name="robots" content="index, follow" />
+      <link rel="canonical" href="https://www.ccseafrica.org/About" />
+
+      
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="About ACCSE – Empowering Youth & Women in Climate Action" />
+      <meta  property="og:description" content="Discover ACCSE's mission, vision, and values — committed to creating sustainable, resilient communities through climate innovation." />
+      <meta property="og:image" content="https://www.ccseafrica.org/assets/aboutPageAboutSection-De6-Ayuz.webp" />
+      <meta property="og:url" content="https://www.ccseafrica.org/About" />
+      <meta property="og:site_name" content="ACCSE" />
+
+      </Helmet>
+
 
         <Header />        
         <div className="mainContainer">            
--- accse/src/pages/Contact.tsx
+++ accse/src/pages/Contact.tsx
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet-async';
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import ContactUs from "../components/contactUs";
@@ -6,6 +7,25 @@ import ContactUs from "../components/contactUs";
 function Contact() {
     return(
         <>
+
+            <Helmet>
+      <title>Contact ACCSE - Get in Touch With Us</title>
+      <meta name="description" content="Reach out to the Africa Center for Climate and Sustainability Empowerment (ACCSE) to collaborate, volunteer, or learn more about our climate programs in Ghana." />
+      <meta name="keywords" content="contact ACCSE, get in touch, climate nonprofit Ghana, contact youth climate organization, sustainability contact Ghana, climate support" />
+      <meta name="author" content="Africa Center for Climate and Sustainability Empowerment" />
+      <meta name="robots" content="index, follow" />
+      <link rel="canonical" href="https://www.ccseafrica.org/Contact" />
+
+      {/* Open Graph */}
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Contact ACCSE - Youth & Climate Empowerment in Ghana" />
+      <meta property="og:description" content="Get in touch with ACCSE to support, collaborate, or learn about our climate empowerment initiatives." />
+      <meta property="og:image" content="https://www.ccseafrica.org/assets/logo-DG_O7rUd.webp" />
+      <meta property="og:url" content="https://www.ccseafrica.org/Contact" />
+      <meta property="og:site_name" content="ACCSE" />
+
+        </Helmet>
+
             <Header />
             <div className="mainContainer">
               <ContactUs />
