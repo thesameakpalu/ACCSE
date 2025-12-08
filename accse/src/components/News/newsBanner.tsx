175a927dfabc1c2c42e9e39646fed1f0b7f185a2
thesameakpalu <150672276+thesameakpalu@users.noreply.github.com>
2025-12-08 10:40:29 +0000

    Update newsBanner.tsx

--- accse/src/components/News/newsBanner.tsx
+++ accse/src/components/News/newsBanner.tsx
@@ -18,7 +18,8 @@ function NewsBanner() {
     <div className="absolute inset-0 z-20 flex gap-5 items-center justify-start pt-30 px-6 ml-10 text-white">
       {/* Left Text */}
       <div className=" flex justify-center items-center">
-        <p className="fade-in-up opacity-0 md:text-xl lg:text-3xl 2xl:text-4xl mt-1 font-bold font-Inter uppercase" style={{ animationDelay: '0.5s' }}>
+        <p className="fade-in-up opacity-0 text-lg md:text-2xl lg:text-3xl xl:text-4xl font-bold font-Inter uppercase leading-tight max-w-[650px]"
+            style={{ animationDelay: '0.5s' }} >
          Follow our journey as we  empower communities and  drive local climate  solutions.
         </p>
       </div>
