ae25e6cb9066868af843389c3c6844664f834fc3
thesameakpalu <braemmanuel89@gmail.com>
2026-01-08 07:18:36 -0800

    First modification after setting up new PC

--- accse/src/components/About/aboutSection.tsx
+++ accse/src/components/About/aboutSection.tsx
@@ -48,18 +48,18 @@ function AboutSection () {
                             </p>
                         </div>
 
-                        <div className="flex items-center justify-center mt-5 2xl:mt-10 2xl:gap-72">
+                        <div className="flex items-center justify-center mt-5 2xl:mt-10 ">
                             <img src={img} alt='Some of Our Climate Leaders' className='w-1/2'></img>
                             
                             <div className='w-1/2  lg:space-y-20 md:space-y-10 lg:p-10 md:p-5  xl:text-lg lg:text-lg md:text-base 2xl:text-2xl'>
                                     <p className='font-Inter font-medium'>
-                                    ACCSE was established to build and support <br className='md:hidden xl:block' /> local capacity among climate leaders to <br className='md:hidden xl:block' /> produce 
-                                    and create locally adaptive innovative <br className='md:hidden xl:block' /> solutions to climate change mitigation and <br className='md:hidden xl:block' />  adaptation.
+                                    ACCSE was established to build and support local capacity among climate leaders to  produce 
+                                    and create locally adaptive innovative  solutions to climate change mitigation and   adaptation.
                                     </p>
 
                                     <p className='font-Inter font-medium'>
-                                         At ACCSE, we challenge the mindset of our <br className='md:hidden xl:block' />  entrepreneurs and Innovators to come up with <br className='md:hidden xl:block' />
-                                          self-sustaining innovations having in mind the <br className='md:hidden xl:block' /> Economic, Social and Environmental Impacts.
+                                         At ACCSE, we challenge the mindset of our <br className='md:hidden xl:block' />  entrepreneurs and Innovators to come up with 
+                                          self-sustaining innovations having in mind the  Economic, Social and Environmental Impacts.
                                     </p>
                             </div>
 
